03c1916185c2cd12710c98c95b1287ba3bd50aeb
suzy birmingham <suzy@suzybirmingham.com>
2014-10-06 11:34:55 -0400

    first commit

---
+++ homework6.rb
@@ -0,0 +1,30 @@
+## Homework 6
+## Do you have the Real Ultimate Power?
+## Create a class Monk which can meditate on life, the universe or everything. It should have three methods 
+## meditate_on_life, meditate_on_the_universe &# meditate_on_everything and returns strings of "I know the 
+## meaning of life", "I know the meaning of the universe", and "I know the meaning of everything", respectively.
+#
+#
+
+class Monk
+	def meditate_on_life 
+		puts "I know the meaning of life."
+	end
+
+	def meditate_on_the_universe 
+		puts "I know the meaning of the universe."
+	end
+
+	def meditate_on_everything 
+		puts "I know the meaning of everything."
+	end
+end
+
+Monk.new.meditate_on_life
+Monk.new.meditate_on_the_universe
+Monk.new.meditate_on_everything
+
+
+
+
+
